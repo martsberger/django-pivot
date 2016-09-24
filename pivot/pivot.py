df0fb2dddba480235ce07aef85ef4a73aaebae1c
martsberger <bmarts@procuredhealth.com>
2016-09-23 20:49:24 -0400

    Allow first argument to pivot to be a Model, QuerySet, or Manager. Extract _get_column_choices into its own function.

--- pivot/pivot.py
+++ pivot/pivot.py
@@ -1,7 +1,8 @@
 from django.db.models import Case, When, Q, F, Sum
+from django.shortcuts import _get_queryset
 
 
-def pivot(queryset, row, column, data):
+def pivot(queryset, row, column, data, aggregation=Sum):
     """
     Takes a queryset and pivots it. The result is a table with one record
     per unique value in the row column, a column for each unique value in the column column
@@ -13,12 +14,17 @@ def pivot(queryset, row, column, data):
     :param data:
     :return: ValuesQueryset
     """
+    queryset = _get_queryset(queryset)
 
-    columns = queryset.values_list(column, flat=True).distinct()
+    columns = _get_column_choices(queryset, column).order_by(column)
 
     annotations = {
-        c: Sum(Case(When(Q(**{column: c}), then=F(data))))
+        c: aggregation(Case(When(Q(**{column: c}), then=F(data))))
         for c in columns
     }
 
     return queryset.values(row).annotate(**annotations)
+
+
+def _get_column_choices(queryset, column):
+    return queryset.values_list(column, flat=True).distinct()
