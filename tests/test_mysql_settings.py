e36cb8939eba5edf3210b3b33a69e295bac696dc
martsberger <bmarts@procuredhealth.com>
2016-09-26 07:04:14 -0400

    Add settings files for mysql and postgres

---
+++ tests/test_mysql_settings.py
@@ -0,0 +1,19 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pivot',
+    }
+}
+
+INSTALLED_APPS = (
+    'tests.pivot',
+)
+
+SITE_ID=1,
+
+SECRET_KEY='secret'
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+)
