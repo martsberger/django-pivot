dab2ca4d3e63453cbe468059e34b50147bdcbe00
martsberger <bmarts@procuredhealth.com>
2017-10-01 23:15:20 -0400

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-pivot',
+    version='1.0',
+    description='Create pivot tables from ORM querysets',
+    url='https://github.com/martsberger/django-pivot',
+    download_url='https://github.com/martsberger/django-pivot/archive/1.0.tar.gz',
+    author='Brad Martsberger',
+    author_email='bmarts@procuredhealth.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.6'
+    ],
+    packages=find_packages(),
+    install_requires=['django>=1.8']
+)
