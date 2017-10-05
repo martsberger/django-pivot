dd3bab54758343baf135e2b60692c9977ee98b86
martsberger <bmarts@procuredhealth.com>
2017-10-05 17:37:09 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-pivot',
-    version='1.1',
+    version='1.2',
     description='Create pivot tables from ORM querysets',
     url='https://github.com/martsberger/django-pivot',
-    download_url='https://github.com/martsberger/django-pivot/archive/1.1.tar.gz',
+    download_url='https://github.com/martsberger/django-pivot/archive/1.2.tar.gz',
     author='Brad Martsberger',
     author_email='bmarts@procuredhealth.com',
     license='MIT',
