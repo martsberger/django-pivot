df3a7c0727a43f156fe37ef130a3f2ee6249b447
martsberger <bmarts@procuredhealth.com>
2017-10-17 23:59:25 -0400

    Allow slice_on an annotated field

--- django_pivot/utils.py
+++ django_pivot/utils.py
@@ -1,9 +1,10 @@
+from django.core.exceptions import FieldDoesNotExist
 from django.utils.encoding import force_text
 
 
 def get_column_values(queryset, field, choices):
     field_object = _get_field(queryset.model, field)
-    if choices == 'auto' and field_object.choices:
+    if choices == 'auto' and getattr(field_object, 'choices', None):
         field_values = field_object.choices
     elif choices == 'auto' or choices == 'minimum':
         database_values = queryset.values_list(field, flat=True).distinct().order_by(field)
@@ -17,7 +18,11 @@ def get_column_values(queryset, field, choices):
 def _get_field(model, field_names):
     field_list = field_names.split('__')
     for field_name in field_list:
-        field = model._meta.get_field(field_name)
+        try:
+            field = model._meta.get_field(field_name)
+        except FieldDoesNotExist:
+            # This can happen when we slice_on an annotated field
+            return None
         if field.is_relation:
             model = field.related_model
         else:
