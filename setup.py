487b157bd567191328f399ae288eeb27fde40192
martsberger <bmarts@procuredhealth.com>
2017-10-26 21:29:57 -0500

    Bump the version to 1.6

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-pivot',
-    version='1.5',
+    version='1.6',
     description='Create pivot tables and histograms from ORM querysets',
     url='https://github.com/martsberger/django-pivot',
-    download_url='https://github.com/martsberger/django-pivot/archive/1.5.tar.gz',
+    download_url='https://github.com/martsberger/django-pivot/archive/1.6.tar.gz',
     author='Brad Martsberger',
     author_email='bmarts@procuredhealth.com',
     license='MIT',
