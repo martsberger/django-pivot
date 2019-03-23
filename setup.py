7d42e1582332feab658a09f85c1d6d1adc74f89f
martsberger <bmarts@lumere.com>
2019-03-23 13:11:24 -0400

    Bump versio to 1.8

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 from setuptools import setup, find_packages
 
+
+def read_file(name):
+    with open(name) as fd:
+        return fd.read()
+
+
 setup(
     name='django-pivot',
-    version='1.7',
+    version='1.8.0',
     description='Create pivot tables and histograms from ORM querysets',
+    long_description=read_file('README.rst'),
     url='https://github.com/martsberger/django-pivot',
-    download_url='https://github.com/martsberger/django-pivot/archive/1.7.tar.gz',
+    download_url='https://github.com/martsberger/django-pivot/archive/1.8.0.tar.gz',
     author='Brad Martsberger',
-    author_email='bmarts@procuredhealth.com',
+    author_email='bmarts@lumere.com',
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     packages=find_packages(),
     install_requires=['django>=1.10', 'six']
