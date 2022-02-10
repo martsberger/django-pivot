c504aeee8c73a0ec42ac24af656b53b7ead881f3
martsberger <bmarts@lumere.com>
2022-02-10 15:25:34 -0500

    set postgres user name

--- django_pivot/tests/test_postgres_settings.py
+++ django_pivot/tests/test_postgres_settings.py
@@ -5,8 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pivot',
         'USER': 'root',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgres'
+        'PASSWORD': 'postgres'
     }
 }
 
